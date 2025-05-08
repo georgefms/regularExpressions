62154dae1299e38b9b96b02166817c2cfd315bb9
George Felipe de Magalhães Silva <georgef@ufpa.br>
2025-05-08 11:20:25 -0300

    initial commit

---
+++ src/main/java/com/georgef/regularexpression/RegularExpression.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.georgef.regularexpression;
+
+/**
+ *
+ * @author georgef
+ */
+public class RegularExpression {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
